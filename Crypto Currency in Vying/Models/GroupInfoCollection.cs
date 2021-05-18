6b68aacdc1fe2c7bb7b2fdce3500d919f5fde588
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2021-05-18 23:00:31 +0200

    Added Data Grid Sorting

---
+++ Crypto Currency in Vying/Models/GroupInfoCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto_Currency_in_Vying.Models
+{
+    public sealed class GroupInfoCollection<T>
+    {
+        public object Key { get; set; }
+        public List<T> Items = new List<T>();
+
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+    }
+}
--- Crypto Currency in Vying/View/MainPage.xaml.cs
+++ Crypto Currency in Vying/View/MainPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+﻿using Crypto_Currency_in_Vying.Models;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.Toolkit.Uwp.UI.Controls;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -33,6 +36,40 @@ namespace Crypto_Currency_in_Vying
 
         public CoinsViewModel ViewModel => (CoinsViewModel)DataContext;
 
-        
+        private void CoinsDataGrid_Sorting(object sender, DataGridColumnEventArgs e)
+        {
+            if (e.Column.Tag.ToString() == "Value")
+            {
+                //Use the Tag property to pass the bound column name for the sorting implementation
+                if (e.Column.Tag.ToString() == "Value")
+                {
+                    //Implement sort on the column "Range" using LINQ
+                    if (e.Column.SortDirection == null || e.Column.SortDirection == DataGridSortDirection.Descending)
+                    {
+                        CoinsDataGrid.ItemsSource = new ObservableCollection<Coin>(from item in ViewModel.Coins
+                                                                                   orderby item.Value ascending
+                                                                                   select item);
+                        e.Column.SortDirection = DataGridSortDirection.Ascending;
+                    }
+                    else
+                    {
+                        CoinsDataGrid.ItemsSource = new ObservableCollection<Coin>(from item in ViewModel.Coins
+                                                                                   orderby item.Value descending
+                                                                                   select item);
+                        e.Column.SortDirection = DataGridSortDirection.Descending;
+                    }
+                }
+                // add code to handle sorting by other columns as required
+
+                // Remove sorting indicators from other columns
+                foreach (var dgColumn in CoinsDataGrid.Columns)
+                {
+                    if (dgColumn.Tag.ToString() != e.Column.Tag.ToString())
+                    {
+                        dgColumn.SortDirection = null;
+                    }
+                }
+            }
+        }
     }
 }
